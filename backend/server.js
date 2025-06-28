f49f95c0e9b76b013cab1b07dcc10010b47a6827
chandanyadav2004 <demo8788151949@gmail.com>
2025-06-28 11:08:04 +0530

    auth route

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+const app = express()
+
+app.get("/api/auth/signup" , (req,res)=>{
+    res.send("Signup Route");
+})
+
+app.get("/api/auth/login" , (req,res)=>{
+    res.send("Login Route");
+})
+
+app.get("/api/auth/logout" , (req,res)=>{
+    res.send("Logout Route");
+})
+
+app.listen(5001,()=>{
+    console.log("Server is running in this port 5001")
+})
