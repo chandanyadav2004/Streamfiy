49b2e80dec61a9dc0c3693f26e01546de4b91817
chandanyadav2004 <demo8788151949@gmail.com>
2025-06-28 11:42:57 +0530

    Route and authentication

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,21 +3,16 @@ import dotenv from "dotenv"; // use this or this import "dotenv/config"
 
 dotenv.config()
 
+
+import authRoutes from "./routes/auth_route.js"
+
 const app = express()
 const PORT = process.env.PORT
 
 // Authenication routing 
-app.get("/api/auth/signup" , (req,res)=>{
-    res.send("Signup Route");
-})
 
-app.get("/api/auth/login" , (req,res)=>{
-    res.send("Login Route");
-})
 
-app.get("/api/auth/logout" , (req,res)=>{
-    res.send("Logout Route");
-})
+app.use("/api/auth",authRoutes)
 
 app.listen(PORT,()=>{
     console.log(`Server is running in this port  ${PORT}`)
