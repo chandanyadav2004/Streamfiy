f1113edcacfff21d72eb58ccf087afe87c8b3406
chandanyadav2004 <demo8788151949@gmail.com>
2025-06-29 10:46:52 +0530

    sendfriend request

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,6 +1,10 @@
 import expres from "express";
 import { protectRoute } from "../middlewares/auth_middleware.js";
-import { getMyFriends, getRecommendUsers } from "../controllers/user_controller.js";
+import {
+  getMyFriends,
+  getRecommendUsers,
+  sendFriendRequest,
+} from "../controllers/user_controller.js";
 
 const router = expres.Router();
 
@@ -10,6 +14,6 @@ router.use(protectRoute);
 router.get("/", getRecommendUsers);
 router.get("/friends", getMyFriends);
 
-
+router.post("/friend-request/:id", sendFriendRequest);
 
 export default router;
